1ac95df8b9300b124671389a381a2e0b8f54ee9c
Hortigas <48776036+Hortigas@users.noreply.github.com>
2020-07-13 16:57:15 -0300

    3-1 done
    criando servidor com node

---
+++ Modulo03/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+nunjucks.configure('views', {
+    express: server,
+});
+
+server.get('/', (req, res) => res.render('homePage'));
+
+server.use(function (req, res) {
+    res.status(404).render('not-found');
+});
+
+server.listen(3000, () => console.log('server is running'));
